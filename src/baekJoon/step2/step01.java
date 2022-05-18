0659182d3438d7464f5700be340734366e54af21
백민수 <iphonex@kakao.com>
2022-05-18 21:13:49 +0900

    단계별 문제 2-1

---
+++ src/baekJoon/step2/step01.java
@@ -0,0 +1,22 @@
+package baekJoon.step2;
+
+public class step01 {
+
+    public static void main(String[] args) {
+
+        // 2단계 1번 문제 - 두 수 비교하기
+
+        int a = 10;
+        int b = 2;
+
+        if( a > b ){
+            System.out.println(">");
+        }else if(a < b){
+            System.out.println("<");
+        }else{
+            System.out.println("==");
+        }
+
+    }
+
+}
