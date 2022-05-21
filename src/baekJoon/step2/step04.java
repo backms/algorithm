7a8b795be5f5fbf02bbc9de9bb5b496b718ef242
백민수 <iphonex@kakao.com>
2022-05-21 22:40:07 +0900

    단계별 2-4문제

---
+++ src/baekJoon/step2/step04.java
@@ -0,0 +1,24 @@
+package baekJoon.step2;
+
+public class step04 {
+
+    public static void main(String[] args) {
+
+        // 2단계 4번 문제 - 사분면 고르기
+
+        int a = 9;
+        int b = -13;
+
+        if(a > 0 && b > 0){
+            System.out.println("1");
+        }else if(a > 0 && b < 0){
+            System.out.println("4");
+        }else if(a < 0 && b > 0){
+            System.out.println("2");
+        }else if(a < 0 && b < 0){
+            System.out.println("3");
+        }
+
+    }
+
+}
